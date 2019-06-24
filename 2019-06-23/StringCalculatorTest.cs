274fe089c5c52de1a7f400bcb1f26394263c512d
Carlos Herasme <rcherasmeestrada@gmail.com>
2019-06-23 22:22:38 -0400

    Update StringCalculatorTest.cs

--- 2019-06-23/StringCalculatorTest.cs
+++ 2019-06-23/StringCalculatorTest.cs
@@ -1,5 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using StringCalculator2;
+using StringCalculatorKata;
 
 namespace StringCalculatorKataTest
 {
@@ -7,75 +7,76 @@ namespace StringCalculatorKataTest
     public class StringCalculatorTest
     {
         [TestMethod]
-        public void SumStringNumbers_EmptyString_Zero()
+        public void SumNumbers_EmptyString_Zero()
         {
             var data = "";
             var wanted = "0";
             var calculator = new StringCalculator();
 
-            var result = calculator.SumStringNumbers(data);
+            var result = calculator.SumNumbers(data);
 
-            Assert.AreEqual(result, wanted);
+            Assert.AreEqual(wanted, result);
         }
 
         [TestMethod]
-        public void SumStringNumbers_OneNumber_Number()
+        public void SumNumbers_OneNumberInString_Number()
         {
             var data = "1";
             var wanted = "1";
             var calculator = new StringCalculator();
 
-            var result = calculator.SumStringNumbers(data);
+            var result = calculator.SumNumbers(data);
 
-            Assert.AreEqual(result, wanted);
+            Assert.AreEqual(wanted, result);
         }
 
         [TestMethod]
-        public void SumStringNumbers_TwoNumber_SumOfNumbers()
+        public void SumNumbers_TwoNumbersInString_SumOfNumbers()
         {
             var data = "1,2";
             var wanted = "3";
             var calculator = new StringCalculator();
 
-            var result = calculator.SumStringNumbers(data);
+            var result = calculator.SumNumbers(data);
 
-            Assert.AreEqual(result, wanted);
+            Assert.AreEqual(wanted, result);
         }
 
         [TestMethod]
-        public void SumStringNumbers_TwoNumberWithNewLineSeparator_SumOfNumbers()
+        public void SumNumbers_TwoArgsOfTwoNumbers_SumOfTwoPairs()
         {
-            var data = "1,2,3";
-            var wanted = "6";
+            var data = "1,2";
+            var data2 = "3,4";
+            var wanted = "3, 7";
             var calculator = new StringCalculator();
 
-            var result = calculator.SumStringNumbers(data);
+            var result = calculator.SumNumbers(data, data2);
 
-            Assert.AreEqual(result, wanted);
+            Assert.AreEqual(wanted, result);
         }
 
         [TestMethod]
-        public void SumStringNumbers_EndsWithComma_ErrorMessage()
+        public void SumNumbers_ThreeNumbersWithCommaAndnewLineSeparators_SumOfNumbers()
+        {
+            var data = "1.1,2\n3";
+            var wanted = "6.1";
+            var calculator = new StringCalculator();
+
+            var result = calculator.SumNumbers(data);
+
+            Assert.AreEqual(wanted, result);
+        }
+
+        [TestMethod]
+        public void SumNumbers_CommaAtTheEnd_ErrorMessage()
         {
             var data = "1,2,";
             var wanted = "Number expected but EOF found.";
             var calculator = new StringCalculator();
 
-            var result = calculator.SumStringNumbers(data);
+            var result = calculator.SumNumbers(data);
 
-            Assert.AreEqual(result, wanted);
-        }
-
-        [TestMethod]
-        public void SumStringNumbers_NegativeNumber_ErrorMessage()
-        {
-            var data = "1,-3,-4,-5";
-            var wanted = "Negative not allowed : -3, -4, -5";
-            var calculator = new StringCalculator();
-
-            var result = calculator.SumStringNumbers(data);
-
-            Assert.AreEqual(result, wanted);
+            Assert.AreEqual(wanted, result);
         }
     }
 }
